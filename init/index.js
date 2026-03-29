cf2c19a404fac4e617d43b86c164cbdc7a42ed92
PrateekPandey18 <prateekpandey437@gmail.com>
2026-03-29 14:13:50 +0530

    Initial commit

---
+++ init/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+main().then(() => {
+    console.log("connected to db")
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/WanderLust');
+}
+
+const initDB = async ()=>{
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({...obj,owner:'69555296fe2dbd3a33a4ad4e'}))
+    await Listing.insertMany(initData.data);
+    console.log("data was initialised")
+}
+initDB();
